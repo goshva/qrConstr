99d7d88f9e27d53da070255cbd1a1d327b688cde
goshva <sha.egor@gmail.com>
2022-08-01 18:27:19 +0300

    fix bags

--- themes/snakeword/static/js/genRandom.js
+++ themes/snakeword/static/js/genRandom.js
@@ -1,4 +1,4 @@
-var edge = 5;
+//var edge = 5;
 var letters = [];
 var gameplace = document.getElementById('gameplace');
 var result = document.getElementById('result');
@@ -11,7 +11,6 @@ function generator(edge){
   }
   genArea(letters,edge);
 }
-
 var subarray = []; //массив в который будет выведен результат.
 function pretty(letters,size){
 	for (let i = 0; i < Math.ceil(letters.length/size); i++){
@@ -28,29 +27,36 @@ function genArea(letters,edge){
 			divbtn.style.zIndex = '10';
 			divbtn.Letter = letters[L]
 			divbtn.LetterCount=L;
-			divbtn.onclick= function (e){
-				switch(this.className){
-					case 'cell': this.className = 'cell activeCell'; break;
-					case 'endCell': this.className = 'endCell activeCell';break;
-					case 'cell activeCell': this.className = 'cell';break;
-					case 'endCell activeCell': this.className = 'endCell';break;
-				}
-				result.innerHTML = JSON.stringify(pretty(letters,edge))
-				letters[this.dataset.number]==0 ? letters[this.dataset.number] = 1 : letters[this.dataset.number] = 0
-				}
-		var t = document.createTextNode('');       
-		divbtn.appendChild(t);                       
 		gameplace.appendChild(divbtn);
     }
+	Eventer(edge)
 }
 document.getElementById('qrEdge').addEventListener('change', cleaner);
-function cleaner() {
+function cleaner(event) {
+	subarray = []
+	var edge = event.target.value
 	while(gameplace.firstChild){
 		gameplace.removeChild(gameplace.firstChild);
 	}	
 	var edge = document.getElementById('qrEdge').value;
 	generator(parseInt(edge))
   }
-
+//
+function Eventer(edge){
+	const childern = gameplace.childNodes;
+	childern.forEach(div => {
+		div.addEventListener("click", function(){
+			letters[this.dataset.number]==0 ? letters[this.dataset.number] = 1 : letters[this.dataset.number] = 0
+			result.innerHTML = JSON.stringify(pretty(letters,edge))
+			switch(this.className){
+				case 'cell': this.className = 'cell activeCell'; break;
+				case 'endCell': this.className = 'endCell activeCell';break;
+				case 'cell activeCell': this.className = 'cell';break;
+				case 'endCell activeCell': this.className = 'endCell';break;
+			}
+		})
+	});
+}
+//
  	  
-generator(edge);
+generator(5);
