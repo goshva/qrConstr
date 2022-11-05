de16c5c9da147c4e9ceb0c0f5ba506154db03117
goshva <sha.egor@gmail.com>
2022-11-05 23:43:42 +0300

    ad fix

--- docs/js/edit.js
+++ docs/js/edit.js
@@ -1,88 +1,97 @@
-//var edge = 5;
 var letters = [];
 var gameplace = document.getElementById('gameplace');
 var result = document.getElementById('result');
 
-function generator(edge){
-  letters = []
-  for (var i= 0; i< (edge*edge); i++){
-	var letter = 0
-	letters.push(letter)
-  }
-  genArea(letters,edge);
-}
-var subarray = []; //массив в который будет выведен результат.
-function pretty(letters,size){
-	for (let i = 0; i < Math.ceil(letters.length/size); i++){
-		subarray[i] = letters.slice((i*size), (i*size) + size);
-	}
-	return subarray
-}
-function genArea(letters,edge){
-    for (var L= 0; L< letters.length; L++){
-		var divbtn = document.createElement('DIV');        
-			(L+1) % edge == 0 ? divbtn.className= 'endCell' : divbtn.className= 'cell';
-			divbtn.id= 'C'+L;
-			divbtn.dataset.number= L;
-			divbtn.style.zIndex = '10';
-			divbtn.Letter = letters[L]
-			divbtn.LetterCount=L;
-		gameplace.appendChild(divbtn);
+function generator(edge) {
+    letters = []
+    for (var i = 0; i < (edge * edge); i++) {
+        var letter = subarray[i] ? subarray[i] : 0
+        letters.push(letter)
     }
-	Eventer(edge)
+    genArea(letters, edge);
+}
+
+function pretty(letters, size) {
+    for (let i = 0; i < Math.ceil(letters.length / size); i++) {
+        subarray[i] = letters.slice((i * size), (i * size) + size);
+    }
+    return subarray
+}
+function genArea(letters, edge) {
+    for (var L = 0; L < letters.length; L++) {
+        var divbtn = document.createElement('DIV');
+        (L + 1) % edge == 0 ? divbtn.className = 'endCell' : divbtn.className = 'cell';
+        divbtn.id = 'C' + L;
+        divbtn.dataset.number = L;
+        divbtn.style.zIndex = '10';
+        divbtn.Letter = letters[L]
+        divbtn.LetterCount = L;
+        if (subarray[L] == 1 && divbtn.className == 'cell') {
+            divbtn.className = 'cell activeCell'
+        }
+        if (subarray[L] == 1 && divbtn.className == 'endCell') {
+            divbtn.className = 'endCell activeCell'
+        }
+        gameplace.appendChild(divbtn);
+    }
+    Eventer(edge)
 }
 document.getElementById('qrEdge').addEventListener('change', cleaner);
 function cleaner(event) {
-	subarray = []
-	var edge = event.target.value
-	while(gameplace.firstChild){
-		gameplace.removeChild(gameplace.firstChild);
-	}	
-	var edge = document.getElementById('qrEdge').value;
-	generator(parseInt(edge))
-  }
-//
-function Eventer(edge){
-	const childern = gameplace.childNodes;
-	childern.forEach(div => {
-		div.addEventListener("click", function(){
-			letters[this.dataset.number]==0 ? letters[this.dataset.number] = 1 : letters[this.dataset.number] = 0
-			result.innerHTML = JSON.stringify(pretty(letters,edge))
-			switch(this.className){
-				case 'cell': this.className = 'cell activeCell'; break;
-				case 'endCell': this.className = 'endCell activeCell';break;
-				case 'cell activeCell': this.className = 'cell';break;
-				case 'endCell activeCell': this.className = 'endCell';break;
-			}
-		})
-	});
+    subarray = []
+    var edge = event.target.value
+    while (gameplace.firstChild) {
+        gameplace.removeChild(gameplace.firstChild);
+    }
+    var edge = document.getElementById('qrEdge').value;
+    generator(parseInt(edge))
 }
 //
- 	  
-generator(5);
+function Eventer(edge) {
+    const childern = gameplace.childNodes;
+    childern.forEach(div => {
+        div.addEventListener("click", function () {
+            letters[this.dataset.number] == 0 ? letters[this.dataset.number] = 1 : letters[this.dataset.number] = 0
+            result.innerHTML = letters
+            switch (this.className) {
+                case 'cell':
+                    this.className = 'cell activeCell';
+                    break;
+                case 'endCell':
+                    this.className = 'endCell activeCell';
+                    break;
+                case 'cell activeCell':
+                    this.className = 'cell';
+                    break;
+                case 'endCell activeCell':
+                    this.className = 'endCell';
+                    break;
+            }
+        })
+    });
+}
+document.getElementById("send").addEventListener("click", function (e) {
+    e.preventDefault();
+    var token = "5430048154:AAEFptLp8IdbKirOYJzzM3ekyTd2ibVLMNc";
+    var chat_id = '190404167';
+    var msg = `https://goshva.github.io/qrConstr/?${letters}`
+    //var msg = `localhost:1313/?${letters}`
+    var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${msg}&parse_mode=html`;
 
-function send() {
-	async function postData(url = '', data = {}) {
-		// Default options are marked with *
-		const response = await fetch(url, {
-		  method: 'POST', 
-		  mode: 'cors', 
-		  cache: 'no-cache', 
-		  credentials: 'same-origin', 
-		  headers: {
-			'Content-Type': 'application/json'
-					  },
-		  redirect: 'follow', 
-		  referrerPolicy: 'no-referrer', 
-		  body: JSON.stringify(data) 
-		});
-		return await response.json(); 
-	  }
-	  
-	  postData('/post', subarray)
-		.then((data) => {
-		  console.log(data);
-		});	
+    fetch(url).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+    });
+})
+
+if (window.location.search !== "" && window.location.search.split("?")[1] !== "") {
+    var subarray = window.location.search.split("?")[1];
+    subarray = subarray.split(',').join('')
+    console.log()
+    generator(Math.sqrt(subarray.length), subarray);
+} else {
+    var subarray = []; // массив в который будет выведен результат.
+    generator(5);
+
 }
-const el = document.getElementById("send");
-el.addEventListener("click", send, false);
